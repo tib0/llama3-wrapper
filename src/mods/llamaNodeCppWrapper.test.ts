5bad0f5c2d8400e83f71dd8d1f0e311b5b3f09c7
Tib0 <4764729+tib0@users.noreply.github.com>
2024-05-06 21:32:31 +0200

    feat: add error callback and abort controller function

--- src/mods/llamaNodeCppWrapper.test.ts
+++ src/mods/llamaNodeCppWrapper.test.ts
@@ -1,14 +1,14 @@
-
 import * as llamaNodeCpp from './llamaNodeCppWrapper';
 
-jest.mock('./llamaNodeCppWrapper', ()=> {
+jest.mock('./llamaNodeCppWrapper', () => {
   return {
-    LlamaWrapper : jest.fn().mockImplementation(() => { return {} })
-  }
+    LlamaWrapper: jest.fn().mockImplementation(() => {
+      return {};
+    }),
+  };
 });
 
 describe('Llama Node Cpp Wrapper function', () => {
-
   test('LlamaWrapper invoke', () => {
     new llamaNodeCpp.LlamaWrapper();
     expect(llamaNodeCpp.LlamaWrapper).toHaveBeenCalledTimes(1);
