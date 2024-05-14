da34c732d69cae77b76172350045f57485d2c2c4
Tib0 <4764729+tib0@users.noreply.github.com>
2024-05-14 12:47:50 +0200

    Feat: add documentation

--- README.md
+++ README.md
@@ -1,8 +1,100 @@
 ![logo](./.github/llama3-wrapper-banner.png)
 
-Lllama node cpp wrapper using Llama3 underneath.
+# Llama3-Wrapper
 
-# BUILDING FROM SOURCE
+A wrapper class for interacting with a LLaMA model instance loaded locally.
+Use any gguf models, you can find models on [huggingface.co](https://huggingface.co/models?search=gguf).
+This projects is based on llama.cpp by [ggerganov](https://github.com/ggerganov) used via node-llama-cpp by [withcatai](https://github.com/withcatai/).
+
+## METHODS
+
+### Constructor
+
+Creates a new instance of the LlamaWrapper class, assigning it a unique ID and initializing its internal state.
+
+### loadModule()
+
+Loads the LLaMA module required for interacting with the model. Throws an error if the module cannot be loaded.
+
+### loadLlama(gpu?: Gpu)
+
+Loads the LLaMA library instance, optionally specifying a GPU device to use. Throws an error if the module isn't initialized.
+
+### loadModel(modelPath: string)
+
+Loads a specific LLaMA model from the specified modelPath. Throws an error if Llama isn't loaded.
+
+### initSession(systemPrompt: string)
+
+Initializes a new chat session with the wrapped LLaMA model using the specified systemPrompt. Throws an error if the model isn't initialized.
+
+### getStatus()
+
+Returns the current status of the wrapper as an object containing the status and optional message. The status can be one of LlamaStatusType (Uninitialized, Ready, Loading, Generating or Error).
+
+### isReady()
+
+Returns a boolean indicating whether the wrapper is in ready state.
+
+### prompt(message: string, onToken?: (chunk: string) => void)
+
+Generates an answer to the specified message and calls the optional onToken callback function with each generated chunk. Throws an error if the session isn't initialized.
+
+### getHistory()
+
+Retrieves the chat history associated with this wrapper's current session. Throws an error if no session is found.
+
+### setHistory(chatHistoryItem: ChatHistoryItem[]): Promise
+
+Sets the chat history associated with this wrapper's current session to the specified chatHistoryItem array. Throws an error if no session is found.
+
+### getId()
+
+Returns the unique ID assigned to this wrapper instance.
+
+### getInfos()
+
+Returns an object containing various information about the wrapped LLaMA model, including its ID, model filename, train context size, and more.
+
+### disposeSession()
+
+Disposes of the current chat session associated with this wrapper. Throws an error if no session is found.
+
+### clearHistory()
+
+Clears the history of the current chat sequence associated with this wrapper. Throws an error if no session or sequence is found.
+
+## TYPES
+
+### Gpu
+
+The `Gpu` type represents a GPU device to use.
+
+- **auto**: The default value, which means the library will automatically select the best available GPU.
+- **cuda**: A CUDA-enabled GPU.
+- **vulkan**: A VULKAN-enabled GPU.
+- **metal**: A METAL-enabled GPU.
+- **false**: Skip GPU usage.
+
+### LlamaStatusType
+
+The `LlamaStatusType` type defines the possible status values for a llama wrapper instance.
+
+- **uninitialized**: The initial state of a new llama session.
+- **loading**: The session is loading a model or initializing session.
+- **ready**: The session is ready to use.
+- **error**: An error occurred during initialization or loading.
+- **generating**: The session is currently generating a response to a message.
+
+### ChatHistoryItem
+
+The `ChatHistoryItem` type represents a single item in the chat history, which contains the message and its corresponding response. Can be ChatSystemMessage | ChatUserMessage | ChatModelResponse
+
+- **type**: The original message sent by the user. can be 'system', 'user' or 'model'.
+- **text**: Represent the stored user text or system prompt. This property comes from ChatSystemMessage and ChatUserMessage types.
+- **response**: Represent the model answer, comes from ChatModelResponse type.
+
+## BUILDING FROM SOURCE
 
 By following the steps below, you can build and install the module from source code.
 
@@ -38,7 +130,7 @@ cd /path/to/target-project
 pnpm link -g llama3-wrapper
 ```
 
-# CONFIGURATION
+## CONFIGURATION
 
 Add your GGUF model path in a .env file at the root of your project:
 
@@ -46,9 +138,9 @@ Add your GGUF model path in a .env file at the root of your project:
 LLAMA_MODELS_PATH=/Users/me/example/LLM/Models/my-model-file.gguf
 ```
 
-# SAMPLES USAGE
+## EXAMPLE
 
-- Sample chat-like usage in terminal:
+Sample chat-like usage in terminal:
 
 ```ts
 import { type ChatHistoryItem, LlamaWrapper } from 'llama3-wrapper';
@@ -116,3 +208,10 @@ const run = async () => {
 
 run();
 ```
+
+# RESSOURCES
+
+- [https://github.com/withcatai/node-llama-cpp](https://github.com/withcatai/node-llama-cpp): Run AI models locally on your machine with node.js bindings for llama.cpp. Force a JSON schema on the model output on the generation level.
+- [https://github.com/ggerganov/llama.cpp](https://github.com/ggerganov/llama.cpp): official implementation of LLaMA in C++.
+- [https://huggingface.co](https://huggingface.co): let's you explore the models and datasets available on the Hub.
+- [https://github.com/facebookresearch/llama](https://github.com/facebookresearch/llama): official implementation of LLaMA.
--- src/mods/llamaNodeCppWrapper.ts
+++ src/mods/llamaNodeCppWrapper.ts
@@ -1,44 +1,122 @@
 import { type LlamaChatSession, type ChatHistoryItem, type LlamaModel, type Llama } from 'node-llama-cpp';
 import { v4 as uuidv4 } from 'uuid';
 
+/**
+ * Enum representing different GPU types.
+ */
 export type Gpu = false | 'auto' | 'cuda' | 'vulkan' | 'metal' | undefined;
 
+/**
+ * Enum representing the status of a llama node.
+ */
 export type LlamaStatusType = 'uninitialized' | 'ready' | 'loading' | 'generating' | 'error';
 
+/**
+ * Exports a type definition for `ChatHistoryItem` from the 'node-llama-cpp' module.
+ */
 export { type ChatHistoryItem } from 'node-llama-cpp';
 
+/**
+ * Export LlamaStatus interface.
+ * This is used to return the status of a llama node
+ */
 export interface LlamaStatus {
   status: LlamaStatusType;
   message: string;
 }
 
+/**
+ * Interface for LlamaCppInfo
+ * This is used to return information about a llama node.
+ */
 export interface LlamaCppInfo {
+  /**
+   * Unique ID of the running llama process
+   */
   id?: string;
+
+  /**
+   * Information about the model use
+   */
   model?: {
+    /**
+     * File name of the model
+     */
     filename: string;
+    /**
+     * Size of the training context
+     */
     trainContextSize: number;
   };
+
+  /**
+   * Information about the context
+   */
   context?: {
+    /**
+     * Batch size used for training
+     */
     batchSize: number;
+    /**
+     * Total size of the context
+     */
     contextSize: number;
+    /**
+     * Number of sequences left to process
+     */
     sequencesLeft: number;
+    /**
+     * Size of the state
+     */
     stateSize: number;
+    /**
+     * Total number of sequences
+     */
     totalSequences: number;
   };
+
+  /**
+   * Information about Llama
+   */
   llama?: {
+    /**
+     * VRAM state information
+     */
     vramState?: {
+      /**
+       * Total VRAM available
+       */
       total: number;
+      /**
+       * Used VRAM
+       */
       used: number;
+      /**
+       * Free VRAM
+       */
       free: number;
     };
+    /**
+     * List of device names
+     */
     deviceNames?: string[];
   };
 }
 
+/**
+ * Loads and wraps the C++ llamaNode module in a TypeScript-friendly way.
+ *
+ * @returns A promise that resolves to the wrapped C++ llamaNode module.
+ */
 async function llamaModule(): Promise<typeof import('node-llama-cpp')> {
   return (await import('node-llama-cpp')) as typeof import('node-llama-cpp');
 }
 
+/**
+ * LlamaWrapper class
+ *
+ * @class
+ */
 export class LlamaWrapper {
   private id: string;
   private session: LlamaChatSession | undefined;
@@ -48,22 +126,64 @@ export class LlamaWrapper {
   private abortController: AbortController;
   private module: typeof import('node-llama-cpp');
   private llama: Llama;
+
+  /**
+   * Initializes the wrapper with an ID and sets the status to uninitialized.
+   *
+   * @constructor
+   */
+  constructor() {
+    this.setStatus('uninitialized', 'Wrapper not initialized');
+    this.id = uuidv4();
+    this.abortController = new AbortController();
+    this.errorCallback = () => {
+      this.abortController.abort();
+      if (this.session) this.session.dispose();
+    };
+  }
+
+  /**
+   * Sets the status of the wrapper.
+   *
+   * @param {string} status - The new status of the wrapper.
+   * @param {string} [payload] - An optional payload for the status update.
+   */
   private setStatus(status: LlamaStatusType, payload?: string) {
     this.status = { status, message: payload };
   }
 
+  /**
+   * Gets the current status of the wrapper.
+   *
+   * @returns {LlamaStatus}
+   */
   public getStatus() {
     return this.status;
   }
 
+  /**
+   * Checks if the wrapper is ready to use.
+   *
+   * @returns {boolean} Whether the wrapper is ready or not.
+   */
   public isReady() {
     return this.status.status === 'ready';
   }
 
+  /**
+   * Checks if the wrapper is ready to use.
+   *
+   * @returns {boolean} Whether the wrapper is ready or not.
+   */
   public getId() {
     return this.id;
   }
 
+  /**
+   * Gets information about the wrapper's current state.
+   *
+   * @returns {LlamaCppInfo}
+   */
   public getInfos(): LlamaCppInfo {
     let infos = {};
 
@@ -110,6 +230,11 @@ export class LlamaWrapper {
     return infos;
   }
 
+  /**
+   * Disposes of the session.
+   *
+   * @throws {Error} If no session is found.
+   */
   public disposeSession() {
     if (!this.session) {
       this.setStatus('error', String('disposeSession: No session found.'));
@@ -118,6 +243,11 @@ export class LlamaWrapper {
     this.session.dispose();
   }
 
+  /**
+   * Clears the history of the session.
+   *
+   * @throws {Error} If no session or sequence is found.
+   */
   public clearHistory() {
     if (!this.session) {
       this.setStatus('error', String('disposeSession: No session found.'));
@@ -130,6 +260,12 @@ export class LlamaWrapper {
     this.session.sequence.clearHistory();
   }
 
+  /**
+   * Loads the Llama module asynchronously.
+   *
+   * @returns {Promise<void>} A promise that resolves when the loading is complete.
+   * @throws {Error} If an error occurs during loading.
+   */
   async loadModule() {
     try {
       this.setStatus('loading', `Loading module from Node Llama Cpp`);
@@ -142,6 +278,13 @@ export class LlamaWrapper {
     }
   }
 
+  /**
+   * Loads the Llama library asynchronously.
+   *
+   * @param {Gpu} [gpu] - The GPU to use (optional, defaults to 'auto').
+   * @returns {Promise<void>} A promise that resolves when the loading is complete.
+   * @throws {Error} If an error occurs during loading.
+   */
   async loadLlama(gpu?: Gpu) {
     if (!this.module) {
       this.setStatus('error', String('loadLlama: No module found.'));
@@ -166,6 +309,13 @@ export class LlamaWrapper {
     }
   }
 
+  /**
+   * Loads a model asynchronously.
+   *
+   * @param {string} modelPath - The path to the model file.
+   * @returns {Promise<void>} A promise that resolves when the loading is complete.
+   * @throws {Error} If an error occurs during loading.
+   */
   async loadModel(modelPath: string) {
     if (!modelPath || modelPath === '') {
       this.setStatus('error', String('loadModel: No path provided.'));
@@ -195,6 +345,13 @@ export class LlamaWrapper {
     }
   }
 
+  /**
+   * Initializes a session asynchronously.
+   *
+   * @param {string} systemPrompt - The system prompt for the session.
+   * @returns {Promise<void>} A promise that resolves when the initialization is complete.
+   * @throws {Error} If an error occurs during initialization.
+   */
   async initSession(systemPrompt: string) {
     if (!this.model) {
       this.setStatus('error', String('initSession: No model loaded.'));
@@ -222,6 +379,14 @@ export class LlamaWrapper {
     }
   }
 
+  /**
+   * Prompts the Llama library asynchronously and returns the response.
+   *
+   * @param {string} message - The prompt to send to the Llama library.
+   * @param {(chunk: string) => void} [onToken] - A callback function for tokenized responses (optional).
+   * @returns {Promise<string>} A promise that resolves with the response from the Llama library.
+   * @throws {Error} If an error occurs during prompting.
+   */
   public async prompt(message: string, onToken?: (chunk: string) => void): Promise<string> {
     if (this.session === undefined) {
       this.setStatus('error', String('prompt: No session found.'));
@@ -249,6 +414,12 @@ export class LlamaWrapper {
     }
   }
 
+  /**
+   * Retrieves the chat history asynchronously and returns it.
+   *
+   * @returns {Promise<ChatHistoryItem[]>} A promise that resolves with the chat history.
+   * @throws {Error} If an error occurs during history retrieval.
+   */
   public async getHistory(): Promise<ChatHistoryItem[]> {
     if (this.session === undefined) {
       this.setStatus('error', String('getHistory: No session found.'));
@@ -268,6 +439,13 @@ export class LlamaWrapper {
     }
   }
 
+  /**
+   * Sets the chat history asynchronously.
+   *
+   * @param {ChatHistoryItem[]} chatHistoryItems - The new chat history to set.
+   * @returns {Promise<void>} A promise that resolves when the setting is complete.
+   * @throws {Error} If an error occurs during history updating.
+   */
   public async setHistory(chatHistoryItem: ChatHistoryItem[]): Promise<void> {
     if (this.session === undefined) {
       this.setStatus('error', String('getHistory: No session found.'));
@@ -284,15 +462,5 @@ export class LlamaWrapper {
       this.setStatus('error', String('setHistory: ' + err.message));
       throw new Error(err);
     }
-  }
-
-  constructor() {
-    this.setStatus('uninitialized', 'Wrapper not initialized');
-    this.id = uuidv4();
-    this.abortController = new AbortController();
-    this.errorCallback = () => {
-      this.abortController.abort();
-      if (this.session) this.session.dispose();
-    };
   }
 }
